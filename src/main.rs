03c9ef84e282e811b2e5376eadaad6a5d9fba2d9
adumbidiot <nathaniel.daniel23@outlook.com>
2019-06-10 13:49:14 -0700

    Add TODOs

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ use toml::Value;
 
 struct Handler;
 
-impl EventHandler for Handler {
+impl EventHandler for Handler { //TODO: Add logging somehow?
     fn ready(&self, _ctx: Context, _ready: Ready) {
         println!("[INFO] Logged in");
     }
@@ -71,7 +71,7 @@ fn main() {
     println!("[INFO] Loading config.toml...");
     let config = load_config(Path::new("./config.toml")).expect("Could not load config.toml"); // TODO: Move to seperate module?
     let mut client = Client::new(&config.token, Handler).expect("Error creating client");
-    let schoology_client = Arc::from(SchoologyClient::new(
+    let schoology_client = Arc::from(SchoologyClient::new( //TODO: Check if token exists (has_schoology()), then create/add conditonally
         config.schoology.token,
         config.schoology.secret,
     ));
@@ -82,7 +82,7 @@ fn main() {
         .cmd("zalgoify", commands::Zalgoify::new())
         .cmd("vaporwave", commands::Vaporwave::new())
         .cmd("xkcd", commands::Xkcd::new())
-        .cmd("fml", commands::Fml::new())
+        .cmd("fml", commands::Fml::new()) //TODO: Finish formatting command output
         .group("schoology", |g| {
             g.prefixes(vec!["schoology"])
                 .desc("A group with commands accessing the schoology api")
@@ -103,7 +103,7 @@ fn main() {
     client.with_framework(framework);
 
     println!("[INFO] Logging in...");
-    if let Err(why) = client.start() {
+    if let Err(why) = client.start() { //Does this use autosharding?
         println!("[ERROR] {:?}", why);
     }
 
